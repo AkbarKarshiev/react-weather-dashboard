8fc22eeeeeb097662889419c3f174d07f6945119
Akbar Karshiev <akbar.karshiev.97@gmail.com>
2019-09-06 23:51:42 +0500

    Adding Bootstrap Styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -57,72 +57,117 @@ class App extends Component {
 
   render() { 
     return (
-      <div id="info">  
-        {/* dropdown selection */}
-        <div className="dropDown"> 
-          <select className="custom-select custom-select-sm" ref={this.selectInput}>>
-            <option value="tashkent" defaultValue>Tashkent</option>
-            <option value="samarkand">Samarkand</option>
-            <option value="moscow">Moscow</option>
-          </select>
-          <button id="search-btn" onClick={this.handleChoice}>
-            <img src={search} width="15" height="15" alt="search"/>
-          </button>
-          <p>{this.state.location}</p>
-        </div>
-        <div className="wrapper-left">
-          <div id="current-weather">
-            {this.state.currentWeather[4].temp}
-            <span>°C</span>
-          </div>
-          <div id="weather-desc">{this.state.currentWeather[6].summary}</div>
-          <div className="temp-max-min">
-            <div className="max-desc">
-              <div id="max-detail">
-                <i>▲</i>
-                {this.state.currentWeather[5].todayHighLow[0].todayTempHigh}
-                <span>°C</span>
+      <div id="ancestor">
+        <div className="container-fluid">
+          <div className="row">
+            <div id="sidebar" className="col-md-3 col-sm-4 col-xs-12 sidebar">
+              <div className="dropDown"> 
+                <select className="custom-select custom-select-sm" ref={this.selectInput}>>
+                  <option value="tashkent" defaultValue>Tashkent</option>
+                  <option value="samarkand">Samarkand</option>
+                  <option value="moscow">Moscow</option>
+                </select>
+                <button id="search-btn" onClick={this.handleChoice}>
+                  <img src={search} width="15" height="15" alt="search"/>
+                </button>
               </div>
-              <div id="max-summary">at {this.state.currentWeather[5].todayHighLow[1].todayTempHighTime}</div>
-            </div>
-            <div className="min-desc">
-              <div id="min-detail">
-                <i>▼</i>
-                {this.state.currentWeather[5].todayHighLow[2].todayTempLow}
-                <span>°C</span>
-              </div>
-              <div id="min-summary">at {this.state.currentWeather[5].todayHighLow[3].todayTempLowTime}</div>
-            </div>
-          </div>
-          <div className="wrapper-right">
-            <div className="date-time-info">
-              <div id="date-desc">
-                <img src={calendar} alt="calendar" width="20" height="20"/>
-                {this.state.currentWeather[3].time}
-              </div>
-            </div>
-            <div className="locaion-info">
-              <div id="location-desc">
-                <img src={locationSvg} 
-                  width="10.83"
-                  height="15.83"
-                  style={{opacity: 0.9}}
-                  alt="location"/>
-                {this.state.currentWeather[0].full_location}
-                <div id="location-detail" className="mt-1">
-                  Lat: {this.state.currentWeather[1].formatted_lat}
-                  <br/>
-                  Long: {this.state.currentWeather[2].formatted_long}
+              <div if="info">
+                <div className="wrapper-left">
+                  <div id="current-weather">
+                    {this.state.currentWeather[4].temp}
+                    <span>°C</span>
+                  </div>
+                  <div id="weather-desc">{this.state.currentWeather[6].summary}</div>
+                  <div class="temp-max-min">
+                    <div class="max-desc">
+                      <div id="max-detail">
+                        <i>▲</i>
+                        {this.state.currentWeather[5].todayHighLow[0].todayTempHigh}
+                        <span>°C</span>
+                      </div>
+                      <div id="max-summary">at {this.state.currentWeather[5].todayHighLow[1].todayTempHighTime}</div>
+                    </div>
+                    <div class="min-desc">
+                      <div id="min-detail">
+                        <i>▼</i>
+                        {this.state.currentWeather[5].todayHighLow[2].todayTempLow}
+                        <span>°C</span>
+                        </div>
+                        <div id="min-summary">at {this.state.currentWeather[5].todayHighLow[3].todayTempLowTime}</div>
+                    </div>
+                  </div> {/*end of temp-max-min*/}
                 </div>
               </div>
             </div>
           </div>
-        </div>        
-        <Content 
-          tempToday={this.state.tempToday}
-          highlights={this.state.highlights}
-        />
+        </div>
       </div>
+      // <div id="info">  
+      //   {/* dropdown selection */}
+      //   <div className="dropDown"> 
+      //     <select className="custom-select custom-select-sm" ref={this.selectInput}>>
+      //       <option value="tashkent" defaultValue>Tashkent</option>
+      //       <option value="samarkand">Samarkand</option>
+      //       <option value="moscow">Moscow</option>
+      //     </select>
+      //     <button id="search-btn" onClick={this.handleChoice}>
+      //       <img src={search} width="15" height="15" alt="search"/>
+      //     </button>
+      //     <p>{this.state.location}</p>
+      //   </div>
+      //   <div className="wrapper-left">
+          // <div id="current-weather">
+          //   {this.state.currentWeather[4].temp}
+          //   <span>°C</span>
+          // </div>
+      //     <div id="weather-desc">{this.state.currentWeather[6].summary}</div>
+      //     <div className="temp-max-min">
+      //       <div className="max-desc">
+      //         <div id="max-detail">
+      //           <i>▲</i>
+      //           {this.state.currentWeather[5].todayHighLow[0].todayTempHigh}
+      //           <span>°C</span>
+      //         </div>
+      //         <div id="max-summary">at {this.state.currentWeather[5].todayHighLow[1].todayTempHighTime}</div>
+      //       </div>
+      //       <div className="min-desc">
+      //         <div id="min-detail">
+      //           <i>▼</i>
+      //           {this.state.currentWeather[5].todayHighLow[2].todayTempLow}
+      //           <span>°C</span>
+      //         </div>
+      //         <div id="min-summary">at {this.state.currentWeather[5].todayHighLow[3].todayTempLowTime}</div>
+      //       </div>
+      //     </div>
+      //     <div className="wrapper-right">
+      //       <div className="date-time-info">
+      //         <div id="date-desc">
+      //           <img src={calendar} alt="calendar" width="20" height="20"/>
+      //           {this.state.currentWeather[3].time}
+      //         </div>
+      //       </div>
+      //       <div className="locaion-info">
+      //         <div id="location-desc">
+      //           <img src={locationSvg} 
+      //             width="10.83"
+      //             height="15.83"
+      //             style={{opacity: 0.9}}
+      //             alt="location"/>
+      //           {this.state.currentWeather[0].full_location}
+      //           <div id="location-detail" className="mt-1">
+      //             Lat: {this.state.currentWeather[1].formatted_lat}
+      //             <br/>
+      //             Long: {this.state.currentWeather[2].formatted_long}
+      //           </div>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>        
+      //   <Content 
+      //     tempToday={this.state.tempToday}
+      //     highlights={this.state.highlights}
+      //   />
+      // </div>
     );
   }
 
@@ -379,7 +424,10 @@ class App extends Component {
   async componentDidMount() {
     const loc = this.utilFunctions.convertToTitleCase(this.selectInput.current.value);
     await this.setState({location: loc});
-    this.fetchWeatherDataAndOrganizeAllDetails();
+    try {
+      this.fetchWeatherDataAndOrganizeAllDetails();
+    } catch (error) {alert(error);}
+    
   }
 }
 
